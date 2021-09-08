c48ebb4f448b1d01a30c131c286ec8b9bfb5ffe4
Sarmouts <saradis.m06@gmail.com>
2021-09-08 13:45:25 +0300

    Add project files.

---
+++ mRNA bases/mRNA bases.cpp
@@ -0,0 +1,70 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string basePairs(string base)
+{
+	for (int i = 0; i < base.length(); i++)
+	{
+		if (!(base[i] == 'A' || base[i] == 'T' || base[i] == 'C' || base[i] == 'G'))
+		{
+			return "Wrong input, please answer with these bases (A,C,G,T)";
+		}
+	}
+	for (int i = 0; i < base.length(); i++)
+	{
+		if (base[i] == 'A')
+		{
+			base[i] = 'U';
+		}
+		else if (base[i] == 'T')
+		{
+			base[i] = 'A';
+		}
+		else if (base[i] == 'C')
+		{
+			base[i] = 'G';
+		}
+		else if (base[i] == 'G')
+		{
+			base[i] = 'C';
+		}
+	}
+	return base;
+}
+int main()
+{
+	cout << "Welcome to MathFlare's early access DNA chain calculator" << endl;
+	cout << "--------------------------------------------------------" << endl;
+	while (true)
+	{
+		string base;
+		cout << "Please answer with these bases(A, C, G, T)" << endl;
+		cout << "------------------------------------------" << endl;
+		cout << "Enter the type of nucleotides(Their Bases) of the one DNA string: ";
+		cin >> base;
+		if (basePairs(base) != "Wrong input, please answer with these bases (A,C,G,T)")
+		{
+			cout << "The type of nucleotides of the mRNA string: " << basePairs(base) << endl << endl;
+			cout << "mRNA: ";
+			for (int i = 0; i <= base.length(); i++)
+			{
+				if (i == base.length())
+				{
+					cout << endl;
+				}
+				else cout << "|";
+			}
+			cout << "      " << basePairs(base) << endl;
+			cout << "===============================================================================================" << endl;
+		}
+		else
+		{
+			cout << basePairs(base) << endl;
+			cout << "===============================================================================================" << endl;
+		}
+	}
+}
+
